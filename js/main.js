069608306d8d81ba1bf812e3ba1349ff0431b502
Jessica <jhai_alex@hotmail.com>
2016-11-23 16:32:58 -0500

    added toggle on/off function, add toggle function to change background, and text

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+$(document).on('ready', function() {
+
+  $('.switch').on('click', function() {
+    $('button').toggleClass('on off')
+
+  if ( $('button').hasClass('on') ) {
+    $('.status').text("It's so bright in here!");
+  } else {
+    $('.status').text("Hey, who turned off the lights?");
+  }
+
+    $('body').toggleClass('light dark');
 
 
+  })
+});
